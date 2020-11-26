2eda7c4ddea84e133a4c0ceaa23b33c2976c8fe6
icymane <egor.andrianov34@gmail.com>
2020-11-27 01:44:16 +0700

    second

--- main.c
+++ main.c
@@ -2,5 +2,20 @@
 
 int main()
 {
+char var;
+printf("Дарова, поздороваться с тобой?\nДа - Y\nНет - N\n");
+scanf("%c", &var);
+do{
+if(var == 'y')
+{
+printf("Привет, челик\n");
+break;
+}
+if(var == 'n')
+{
+printf("Как хочешь...\n");
+break;
+}
+}while(var != 'y' || var != 'n');
 return 0;
 }
